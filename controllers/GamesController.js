a32e4f151ae2d96c8041ae326dce9f0a2b3fb168
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-07-26 22:22:34 -0300

    created method put

--- controllers/GamesController.js
+++ controllers/GamesController.js
@@ -20,87 +20,142 @@ router.get("/games/:id", (req, res) => {
     if (isNaN(id)) {
         res.sendStatus(400);
     } else {
-        
+
         Game.findOne({
             where: {
                 id: id
             }
         })
-        .then((game) => {
-            if (game != undefined) {
-                res.statusCode = 200;
-                res.json(game);
-            } else {
-                res.sendStatus(404);
-            }
-            
-        })
-        .catch((error) => {
-            res.send(error);
-        });
+            .then((game) => {
+                if (game != undefined) {
+                    res.statusCode = 200;
+                    res.json(game);
+                } else {
+                    res.sendStatus(404);
+                }
+
+            })
+            .catch((error) => {
+                res.send(error);
+            });
     }
-    
+
 });
 
 router.post("/games", (req, res) => {
-    const {name, age, price} = req.body;
+    const { name, age, price } = req.body;
 
     if (!name || !age || isNaN(age) || price < 0 || price == undefined || price == null || isNaN(price)) {
         res.sendStatus(400);
-     } else {
+    } else {
         Game.create({
             name: name,
             age: age,
             price: price
         })
-        .then(() => {
-            res.statusCode = 200;
-            res.send("game created!");
-        })
-        .catch((error) => {
-            res.statusCode = 404;
-            res.send(error);
-        });
+            .then(() => {
+                res.statusCode = 200;
+                res.send("game created!");
+            })
+            .catch((error) => {
+                res.statusCode = 404;
+                res.send(error);
+            });
     }
 });
 
-// router.put("/games", (req, res) => {
-//     const id = req.body.id;
-//     const name = req.body.name;
-//     const age = req.body.age;
-//     const price = req.body.price;
-
-//     Game.update(
-//         {name: name, age: age, price: price},
-//         {where: {id: id}}
-//     )
-//     .then(() => {
-//         res.send("updated sucesfull!");
-//     })
-//     .catch((error) => {
-//         res.send(error);
-//     });
-//     res.send("oi");
-// });
-
-router.delete("/games/:id", (req, res) => {
-    const id = req.params.id;
+router.put("/games/:id", (req, res) => {
+    const id = parseInt(req.params.id);
 
     if (isNaN(id)) {
         res.sendStatus(400);
     } else {
-        Game.destroy({
-            where: {id: id}
-        })
-        .then(() => {
-            res.statusCode = 200;
-            res.send("game deleted!");
-        })
-        .catch((error) => {
-            res.statusCode = 400;
-            res.send(error);
-        });
-    }
+        if (id < 1) {
+            res.sendStatus(400);
+        } else {
+            console.log('passou');
+            Game.findOne({ where: { id: id } })
+                .then((game) => {
+                    if (!game) {
+                        res.sendStatus(404);
+                    } else {
+                        const { name, age, price } = req.body;
+
+                        if (name != undefined) {
+                            Game.update({ name: name }, { where: { id: id } })
+                                .then(() => {
+                                    res.statusCode = 200;
+                                    res.send("game updated!");
+                                })
+                                .catch(() => {
+                                    // res.sendStatus(400);
+                                });
+                        };
+
+                        if (age != undefined) {
+                            Game.update({ age: age }, { where: { id: id } })
+                                .then(() => {
+                                    res.statusCode = 200;
+                                    res.send("game updated!");
+                                })
+                                .catch(() => {
+                                    // res.sendStatus(400);
+                                });
+                        };
+
+                        if (price != undefined) {
+                            Game.update({ price: price }, { where: { id: id } })
+                                .then(() => {
+                                    res.statusCode = 200;
+                                    res.send("game updated!");
+                                })
+                                .catch(() => {
+                                    // res.sendStatus(400);
+                                });
+                        };
+                    };
+                })
+                .catch(() => {
+                    res.sendStatus(400);
+                });
+        };
+    };
+})
+
+router.delete("/games/:id", (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400);
+    } else {
+        const id = parseInt(req.params.id);
+
+        if (id < 1) {
+            res.sendStatus(400);
+        } else {
+            Game.findOne({
+                where: { id: id }
+            })
+                .then((game) => {
+                    if (!game) {
+                        res.sendStatus(404);
+                    } else {
+                        Game.destroy({
+                            where: { id: id }
+                        })
+                            .then(() => {
+                                res.statusCode = 200;
+                                res.send("game deleted!");
+                            })
+                            .catch((error) => {
+                                res.statusCode = 400;
+                                res.send(error);
+                            });
+                    };
+                })
+                .catch((error) => {
+                    res.sendStatus(400);
+                });
+        };
+    };
 });
 
 module.exports = router;
